1344098ff5d743a8b58ae05678fdfcac30e7a2c2
Tomasz A. Biczel <tomasz@biczel.com>
2014-05-23 21:02:24 +0200

    Metric dependencies resolved

--- app/magnify/services/DependenciesResolvedMetricsProvider.scala
+++ app/magnify/services/DependenciesResolvedMetricsProvider.scala
@@ -1,5 +1,6 @@
 package magnify.services
 
+import scala.collection.mutable
 import scala.collection.JavaConversions._
 
 import japa.parser.ast.body.TypeDeclaration
@@ -10,8 +11,31 @@ sealed abstract class DependenciesResolvedMetricsProvider[A, B, C <: Metric[A, B
     metrics: java.util.Set[C])
   extends MetricsProvider[A, B, C] {
 
-  // TODO(biczel): Add dependencies between metrics
-  override def apply(): Seq[C] = metrics.toSeq
+  private val metricByName = metrics.map((metric) => (metric.name -> metric)).toMap
+  private val adding = mutable.Set[String]()
+  private val addedMetrics = mutable.Set[String]()
+
+  override def apply(): Seq[C] = {
+    addedMetrics.clear()
+    adding.clear()
+    metrics.foldLeft(Seq[C]()) { (metricsSeq, metric) =>
+      addMetric(metric, metricsSeq)
+    }
+  }
+
+  private def addMetric(metric: C, metrics: Seq[C]): Seq[C] = if (addedMetrics(metric.name)) { metrics } else {
+    if (adding(metric.name)) { throw new IllegalStateException("Cycle in dependencies for: " + metric.name) }
+    adding.add(metric.name)
+    val dependencies = metric.dependencies.map((dependencyName) => metricByName.getOrElse(dependencyName, {
+      throw new IllegalStateException("Unresolved dependency: " + dependencyName + " for " + metric.name)
+    }))
+    val withDependencies = dependencies.foldLeft(metrics) { (metricsSeq, dependency) =>
+      addMetric(dependency, metricsSeq)
+    }
+    adding.remove(metric.name)
+    addedMetrics.add(metric.name)
+    withDependencies ++ Seq(metric)
+  }
 }
 
 class FullGraphDependenciesResolvedMetricsProvider(
@@ -24,4 +48,4 @@ class RevisionDependenciesResolvedMetricsProvider(
 
 class AstDependenciesResolvedMetricsProvider(
     metrics: java.util.Set[AstMetric])
-    extends DependenciesResolvedMetricsProvider[TypeDeclaration, AnyRef, AstMetric](metrics)
+    extends DependenciesResolvedMetricsProvider[TypeDeclaration, AnyRef , AstMetric](metrics)
